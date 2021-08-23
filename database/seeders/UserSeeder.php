9c6614e71ba0eb8badd89a55337283cc327349d6
Indra Hasan <indra.hasan.072020@gmail.com>
2021-08-23 23:47:59 +0700

    add user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        if (!$this->checkSuperadminExist($user)) {
+            $user->create([
+                'name' => 'Superadmin',
+                'username' => 'superadmin',
+                'email' => 'superadmin@mail.com',
+                'password' => Hash::make('superadmin'),
+            ]);
+        }
+    }
+
+    private function checkSuperadminExist($user): bool
+    {
+        return $user->where(['is_superadmin' => true,'username' => 'superadmin'])->count() > 0;
+    }
+}
